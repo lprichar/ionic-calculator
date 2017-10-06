1e8be1b0650da6b946f50091038d7624d302b12e
Lee Richardson <github@leerichardson.com>
2017-10-06 19:57:55 -0400

    Fixed the left nav items and button heights

--- ionic-calculator/src/app/app.component.ts
+++ ionic-calculator/src/app/app.component.ts
@@ -21,8 +21,10 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Standard', component: Page1 },
+      { title: 'Scientific', component: Page2 },
+      { title: 'Programmer', component: Page2 },
+      { title: 'Date Calculation', component: Page2 }
     ];
 
   }
